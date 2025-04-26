2dd6d0606398ce1c034d16962a01450bb6e7594f
rsandovaljr1 <r.sandovaljr1@gmail.com>
2025-04-26 02:57:19 -0700

    commit all

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import EmployeeForm from './components/EmployeeForm';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>New Employee Form</h1>
+      <EmployeeForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
