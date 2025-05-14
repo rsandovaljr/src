f32d300001f8203f8c2415060c7995d701350a32
rsandoval1 <r.sandovaljr1@gmail.com>
2025-05-13 20:24:57 -0700

    Update App.js
    week 3-4

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import EmployeeList from './components/EmployeeList';
 import EmployeeForm from './components/EmployeeForm';
 import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>New Employee Form</h1>
-      <EmployeeForm />
-      </header>
-    </div>
-  );
+    const [employees, setEmployees] = useState([]);
+
+    //load  data from local storage
+    useEffect(() => {
+        const savedEmployees = localStorage.getItem('employees');
+        if (savedEmployees) {
+            setEmployees(JSON.parse(savedEmployees));
+        }
+    }, []);
+
+    //save data to local storage
+    useEffect(() => {
+        saveData(employees);
+    }, [employees]);
+
+    const saveData = (data) => {
+        localStorage.setItem('employees', JSON.stringify(data));
+    };
+
+    const addEmployee = (employee) => {
+        const newEmployee = { ...employee, EmployeeId: Date.now() };
+        setEmployees([...employees, newEmployee]);
+    };
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <header className="App-header">
+                    <h1>New Employee Form</h1>
+                    <EmployeeForm addEmployee={addEmployee} />
+                    <EmployeeList employees={employees} />
+                    <Routes>
+                        <Route path="/employees/:id" element={<div>Employee Details</div>} />
+                    </Routes>
+                </header>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
+
